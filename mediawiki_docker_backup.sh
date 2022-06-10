30d39e155910f670f0fa54dd0a2233d1aaa795e7
rekyuu <senkan.re.kyuu@gmail.com>
2022-06-09 20:39:39 -0600

    Fix destination bucket

--- mediawiki_docker_backup.sh
+++ mediawiki_docker_backup.sh
@@ -8,6 +8,6 @@ BACKUP_FILENAME=$(docker exec "$CONTAINER_ID" /scripts/mw-backup.sh)
 
 cd "/srv/$MW_NAME/backups" || exit 1
 
-aws s3 mv "$BACKUP_FILENAME" "s3://$S3_DB_BUCKET/$MW_NAME/$BACKUP_FILENAME" --only-show-errors
+aws s3 mv "$BACKUP_FILENAME" "s3://$S3_SITE_BUCKET/$MW_NAME/$BACKUP_FILENAME" --only-show-errors
 
-rm "$BACKUP_FILENAME"
+rm "$BACKUP_FILENAME" 2> /dev/null
