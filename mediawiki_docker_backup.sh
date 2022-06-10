853ad54769a4aa4bda69002c195dbac578a796eb
rekyuu <senkan.re.kyuu@gmail.com>
2022-06-09 20:47:39 -0600

    shrug lol

--- mediawiki_docker_backup.sh
+++ mediawiki_docker_backup.sh
@@ -8,6 +8,8 @@ BACKUP_FILENAME=$(docker exec "$CONTAINER_ID" /scripts/mw-backup.sh)
 
 cd "/srv/$MW_NAME/backups" || exit 1
 
+sudo chown rekyuu:rekyuu "$BACKUP_FILENAME"
+
 aws s3 mv "$BACKUP_FILENAME" "s3://$S3_SITE_BUCKET/$MW_NAME/$BACKUP_FILENAME" --only-show-errors
 
 rm "$BACKUP_FILENAME" 2> /dev/null
